7a42af458ea0bac3ef828b45a5a477571b9ae025
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2019-10-26 13:44:19 -0700

    removed unnecessary android theme in AndroidManifest.xml and FloatingActionButton in MainActivity.java

--- app/src/main/java/com/example/studiableapp/MainActivity.java
+++ app/src/main/java/com/example/studiableapp/MainActivity.java
@@ -34,15 +34,6 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 
         /*button = (Button)findViewById(R.id.button35);
 
