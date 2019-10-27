4357a91d013346b119022328a1bf5e8abe072d63
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2019-10-27 00:03:35 -0700

    fixed toggle switch for flashcards

--- app/src/main/java/com/example/studiableapp/MainActivity.java
+++ app/src/main/java/com/example/studiableapp/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
